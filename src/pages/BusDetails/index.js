12e15f909a2fe32b005b08091bc9d6d72bcdbf78
DS <yeodisheng@gmail.com>
2023-07-04 06:47:59 +0000

    Update 10 files

--- src/pages/BusDetails/index.js
+++ src/pages/BusDetails/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../../assets/BusDetails.css";
+import AddBusDetails from "./addBusDetails";
 
 const BusDetails = () => {
   return (
     <>
-      <h1>BusDetails Page</h1>
+      <h1>Bus Details Page</h1>
       Page to view Past/Current/Future trips
+      <AddBusDetails />
     </>
   );
 };
--- src/pages/DriverDetails/index.js
+++ src/pages/DriverDetails/index.js
@@ -1,10 +1,17 @@
 import React from "react";
+import DriverList from "./driverList.js";
+import AddDriver from "./addDriver.js";
+// import UpdateDriver from "./addDriver.js";
+// import DeleteDriver from "./addDriver.js";
 
 const DriverDetails = () => {
   return (
     <>
       <h1>Driver Details Page</h1>
-      <h1>List of drivers, ability to add/edit/delete</h1>
+      <h2> List of Drivers </h2>
+      <DriverList />
+      <h2>Create Driver</h2>
+      <AddDriver />
     </>
   );
 };
