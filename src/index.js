407fc171833072f77b20ca89f58e6999507e80b7
DS <yeodisheng@gmail.com>
2023-07-08 06:53:36 +0000

    Added Login Logout

--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from ".//components/NavBar/NavBar";
-import Salary from ".//pages/Salary/";
-import Scheduling from ".//pages/Scheduling/";
-import BusDetails from ".//pages/BusDetails/";
-import DriverDetails from ".//pages/DriverDetails/";
-import Login from ".//pages/Login/"
-import "./assets/index.css";
+import App from "./App";
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/salary" element={<Login />} />
-        <Route path="/scheduling" element={<Scheduling />} />
-        <Route path="/bus-details" element={<BusDetails />} />
-        <Route path="/driver-details" element={<DriverDetails />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
