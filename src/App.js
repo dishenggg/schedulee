5b5bc0d3768e43bf41910e993b991a6f1d994883
jethrokyq <jethrokyq@gmail.com>
2023-07-09 00:28:35 +0800

    404 page + route handling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //import React from "react";
 import "./assets/index.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import {ConfigProvider, theme} from 'antd'
 import Navbar from "./components/NavBar/NavBar";
 import HideNavBarInLogin from "./components/NavBar/HideNavBarInLogin";
@@ -10,6 +10,7 @@ import Scheduling from ".//pages/Scheduling/";
 import BusDetails from ".//pages/BusDetails/";
 import DriverDetails from ".//pages/DriverDetails/";
 import Login from ".//pages/Login/";
+import NotFound from "./pages/NotFound";
 
 
 function App() {
@@ -22,13 +23,14 @@ function App() {
 
       <Routes>
         <Route element={<PrivateRoutes />}>
-          <Route path="/" element={<Scheduling />} />
+          <Route exact path="/" element={<Scheduling />} />
           <Route path="/salary" element={<Salary />} />
           <Route path="/scheduling" element={<Scheduling />} />
           <Route path="/bus-details" element={<BusDetails />} />
           <Route path="/driver-details" element={<DriverDetails />} />
         </Route>
         <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
     </ConfigProvider>
--- src/pages/DriverDetails/index.js
+++ src/pages/DriverDetails/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import DriverList from "./driverList.js";
 import AddDriver from "./addDriver.js";
 import {Title} from "../../components/Typography/Title"
+
 // import UpdateDriver from "./addDriver.js";
 // import DeleteDriver from "./addDriver.js";
 
@@ -9,9 +10,9 @@ const DriverDetails = () => {
   return (
     <>
       <Title>Drivers </Title>
-      <DriverList />
-      <h2>Create Driver</h2>
       <AddDriver />
+      <DriverList />
+      
     </>
   );
 };
