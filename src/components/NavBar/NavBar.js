3856a1d87e6f6fbe107571d4f60bd7563f2daf42
Jethro Kiu <37759922+jethrokyq@users.noreply.github.com>
2023-07-08 21:04:29 +0800

    logout button (#13)
    * setup
    
    * update to antd and change navbar
    
    1) npm install antd --save
    2) npm install @ant-design/icons --save
    
    * logout button, username and title component

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,11 +1,16 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { auth } from "../../firebase";
-import { signOut } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
-import {Menu} from 'antd'
-import {DollarOutlined, CalendarOutlined, CarOutlined, IdcardOutlined} from '@ant-design/icons'
-
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { auth } from '../../firebase';
+import { signOut } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import { Menu, Button} from 'antd';
+import {
+  DollarOutlined,
+  CalendarOutlined,
+  CarOutlined,
+  IdcardOutlined,
+  LogoutOutlined,
+} from '@ant-design/icons';
 
 const menuItems = [
   {
@@ -32,23 +37,59 @@ const menuItems = [
 ]
 const Navbar = () => {
   const navigate = useNavigate();
-  const [page, setPage] = useState('salary')
+  const [page, setPage] = useState('salary');
+
   const handleLogout = async (e) => {
     e.preventDefault();
     try {
       await signOut(auth);
-      navigate("/login");
+      navigate('/login');
     } catch (err) {
       console.log(err);
     }
   };
 
+  const menuItems = [
+    {
+      label: <Link to="/salary">Salary</Link>,
+      key: 'salary',
+      icon: <DollarOutlined />,
+    },
+    {
+      label: <Link to="/scheduling">Schedule</Link>,
+      key: 'schedule',
+      icon: <CalendarOutlined />,
+    },
+    {
+      label: <Link to="/bus-details">Bus Details</Link>,
+      key: 'bus',
+      icon: <CarOutlined />,
+    },
+    {
+      label: <Link to="/driver-details">Driver Details</Link>,
+      key: 'driver',
+      icon: <IdcardOutlined />,
+    },
+    {
+      label: <>{auth?.currentUser?.email}</>,
+      style: { marginLeft: 'auto' },
+      children: [
+        {
+          label: (
+            <Button type="ghost" icon={<LogoutOutlined />} onClick={handleLogout}>
+              Log Out
+            </Button>
+          ),
+          key: 'logout',
+        },
+      ],
+    },
+  ];
+
   const onClick = (e) => {
-    setPage(e.key)
+    setPage(e.key);
   };
-  return (
-    <Menu onClick={onClick} selectedKeys={[[page]]} mode='horizontal' items={menuItems}/>
-  );
+  return <Menu onClick={onClick} selectedKeys={[[page]]} mode="horizontal" items={menuItems} />;
 };
 
 export default Navbar;
---
+++ src/components/Typography/Title.js
@@ -0,0 +1,5 @@
+import { Typography } from "antd";
+const {Title} = Typography;
+
+
+export {Title};
