1bab9209082a5b2774397e5ad59864316188fcda
jethrokyq <jethrokyq@gmail.com>
2023-07-08 20:50:01 +0800

    logout button, username and title component

--- src/pages/DriverDetails/index.js
+++ src/pages/DriverDetails/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import DriverList from "./driverList.js";
 import AddDriver from "./addDriver.js";
+import {Title} from "../../components/Typography/Title"
 // import UpdateDriver from "./addDriver.js";
 // import DeleteDriver from "./addDriver.js";
 
 const DriverDetails = () => {
   return (
     <>
-      <h1>Driver Details Page</h1>
-      <h2> List of Drivers </h2>
+      <Title>Drivers </Title>
       <DriverList />
       <h2>Create Driver</h2>
       <AddDriver />
