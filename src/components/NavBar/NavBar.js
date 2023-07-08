e1ede87b28699514b4078ded21c3bec6e356f15c
Jethro Kiu <37759922+jethrokyq@users.noreply.github.com>
2023-07-08 18:43:37 +0800

    Style changes to navbar
    * setup
    
    * update to antd and change navbar
    
    1) npm install antd --save
    2) npm install @ant-design/icons --save

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,10 +3,36 @@ import { Link } from "react-router-dom";
 import { auth } from "../../firebase";
 import { signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import {Menu} from 'antd'
+import {DollarOutlined, CalendarOutlined, CarOutlined, IdcardOutlined} from '@ant-design/icons'
 
+
+const menuItems = [
+  {
+    label: (<Link to="/salary">Salary</Link>),
+    key: 'salary',
+    icon:<DollarOutlined />,
+  },
+  {
+    label: (<Link to="/scheduling">Schedule</Link>),
+    key: 'schedule',
+    icon:<CalendarOutlined />,
+  },
+  {
+    label: (<Link to="/bus-details">Bus Details</Link>),
+    key: 'bus',
+    icon:<CarOutlined />,
+  },
+  {
+    label: (<Link to="/driver-details">Driver Details</Link>),
+    key: 'Driver',
+    icon:<IdcardOutlined />,
+  },
+
+]
 const Navbar = () => {
   const navigate = useNavigate();
-
+  const [page, setPage] = useState('salary')
   const handleLogout = async (e) => {
     e.preventDefault();
     try {
@@ -16,26 +42,12 @@ const Navbar = () => {
       console.log(err);
     }
   };
+
+  const onClick = (e) => {
+    setPage(e.key)
+  };
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/salary">Salary</Link>
-        </li>
-        <li>
-          <Link to="/scheduling">Scheduling</Link>
-        </li>
-        <li>
-          <Link to="/bus-details">Bus Details</Link>
-        </li>
-        <li>
-          <Link to="/driver-details">Driver Details</Link>
-        </li>
-        <li>
-          <Link onClick={handleLogout}>{auth?.currentUser?.email} Log Out</Link>
-        </li>
-      </ul>
-    </nav>
+    <Menu onClick={onClick} selectedKeys={[[page]]} mode='horizontal' items={menuItems}/>
   );
 };
 
