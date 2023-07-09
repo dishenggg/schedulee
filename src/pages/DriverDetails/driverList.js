7268825dcc2239fe339bce07781e3eed5c5f1471
jethrokyq <jethrokyq@gmail.com>
2023-07-09 14:00:16 +0800

    change button design

--- src/pages/DriverDetails/driverList.js
+++ src/pages/DriverDetails/driverList.js
@@ -1,15 +1,10 @@
-import { useEffect, useState, useMemo } from "react";
-import { db } from "../../firebase";
-import {
-  collection,
-  getDocs,
-  deleteDoc,
-  doc,
-  updateDoc,
-} from "firebase/firestore";
-import { AgGridReact } from "ag-grid-react";
-import "ag-grid-community/styles/ag-grid.css";
-import "ag-grid-community/styles/ag-theme-alpine.css";
+import { useEffect, useState, useMemo } from 'react';
+import { db } from '../../firebase';
+import { collection, getDocs, deleteDoc, doc, updateDoc } from 'firebase/firestore';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+import { Button, Popconfirm } from 'antd';
 
 function DriverList() {
   const [drivers, setDrivers] = useState([]);
@@ -25,7 +20,7 @@ function DriverList() {
   }, []);
 
   const fetchDrivers = async () => {
-    const driversRef = collection(db, "Bus Drivers");
+    const driversRef = collection(db, 'Bus Drivers');
     const snapshot = await getDocs(driversRef);
     const driverData = snapshot.docs.map((doc) => ({
       id: doc.id,
@@ -44,42 +39,41 @@ function DriverList() {
       handleDelete(rowData);
     };
 
-    return <button onClick={handleClick}>Delete</button>;
+    return (
+      <Popconfirm
+        title="Delete Driver"
+        description="Confirm Delete Driver?"
+        onConfirm={handleClick}
+      >
+        <Button danger size='small'>Delete</Button>
+      </Popconfirm>
+    );
   };
 
   const handleDelete = (data) => {
     console.log(data);
-    const confirmDelete = window.confirm(
-      "Are you sure you want to delete " + data.id + "?"
-    );
-    if (confirmDelete) {
-      deleteDoc(doc(db, "Bus Drivers", data.id))
+      deleteDoc(doc(db, 'Bus Drivers', data.id))
         .then(() => {
-          alert(data.id + "deleted successfully!");
+          alert(data.id + 'deleted successfully!');
           fetchDrivers();
         })
         .catch((error) => {
           console.log(error);
-          alert("Failed to delete driver.");
+          alert('Failed to delete driver.');
         });
-    }
   };
 
   const validateRow = (row) => {
     if (!/^\d{8}$/i.test(row.contactNumber)) {
-      throw new Error("Contact number should only have 8 digits!");
+      throw new Error('Contact number should only have 8 digits!');
     }
 
     if (!/^\d{3}[a-zA-Z]$/.test(row.icNumber)) {
-      throw new Error("Invalid IC Number. IC Number should be in 123A format.");
+      throw new Error('Invalid IC Number. IC Number should be in 123A format.');
     }
 
-    if (
-      !(row.minSalary > 0) ||
-      !(row.normalSalary > 0) ||
-      !(row.peakHourSalary > 0)
-    ) {
-      throw new Error("Salary must be above 0");
+    if (!(row.minSalary > 0) || !(row.normalSalary > 0) || !(row.peakHourSalary > 0)) {
+      throw new Error('Salary must be above 0');
     }
     return true;
   };
@@ -89,13 +83,11 @@ function DriverList() {
     console.log(updatedData);
     try {
       validateRow(updatedData);
-      const confirmUpdate = window.confirm(
-        "Are you sure you want to Update this?"
-      );
+      const confirmUpdate = window.confirm('Are you sure you want to Update this?');
       if (confirmUpdate) {
-        updateDoc(doc(db, "Bus Drivers", id), updatedData).catch((error) => {
+        updateDoc(doc(db, 'Bus Drivers', id), updatedData).catch((error) => {
           console.log(error);
-          alert("Failed to update driver.");
+          alert('Failed to update driver.');
         });
       }
     } catch (err) {
@@ -108,64 +100,64 @@ function DriverList() {
     var number = params.value;
     var firstFour = number.slice(0, 4);
     var lastFour = number.slice(4, 8);
-    return firstFour + " " + lastFour;
+    return firstFour + ' ' + lastFour;
   };
 
   const columnDefs = [
     {
-      headerName: "Bus Number",
-      field: "busNumber",
+      headerName: 'Bus Number',
+      field: 'busNumber',
       editable: false,
       flex: 2,
     },
     {
-      headerName: "Driver Name",
-      field: "name",
+      headerName: 'Driver Name',
+      field: 'name',
       flex: 4,
     },
     {
-      headerName: "Contact Number",
-      field: "contactNumber",
+      headerName: 'Contact Number',
+      field: 'contactNumber',
       valueFormatter: stringFormatter,
     },
     {
-      headerName: "IC Number",
-      field: "icNumber",
+      headerName: 'IC Number',
+      field: 'icNumber',
       flex: 2,
     },
     {
-      headerName: "Local",
-      field: "local",
+      headerName: 'Local',
+      field: 'local',
       maxWidth: 75,
     },
     {
-      headerName: "Minimum Salary",
-      field: "minSalary",
+      headerName: 'Minimum Salary',
+      field: 'minSalary',
       flex: 2,
     },
     {
-      headerName: "Normal Salary",
-      field: "normalSalary",
+      headerName: 'Normal Salary',
+      field: 'normalSalary',
       flex: 2,
     },
     {
-      headerName: "Peak Hour Salary",
-      field: "peakHourSalary",
+      headerName: 'Peak Hour Salary',
+      field: 'peakHourSalary',
       flex: 2,
     },
     {
-      headerName: "Delete",
-      field: "delete",
+      headerName: 'Delete',
+      field: 'delete',
       editable: false,
       cellRenderer: deleteCellRenderer,
       flex: 2,
       sortable: false,
-      cellClass: "delete-cell",
+      cellClass: 'delete-cell',
     },
   ];
 
   return (
-    <div className="ag-theme-alpine" style={{ height: "400px", width: "100%" }}>
+    <div className="ag-theme-alpine" style={{ height: '400px', width: '100%' }}>
       <AgGridReact
         rowData={drivers}
         columnDefs={columnDefs}
