0f6ca3d8bb52c25244ffe5ca3c6057fae1b826ca
DS <yeodisheng@gmail.com>
2023-07-11 08:35:29 +0000

    Drag n drop v1

--- src/pages/Scheduling/SchedulingApp.js
+++ src/pages/Scheduling/SchedulingApp.js
@@ -1,122 +1,140 @@
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { db } from "../../firebase";
-import { collection, getDocs, where, query } from "firebase/firestore";
+import { collection, getDocs, doc, updateDoc } from "firebase/firestore";
 
-export default function SchedulingApp({ selectedDate }) {
-  const [listOfDrivers, setListOfDrivers] = useState([]);
-  const [listOfTrips, setListOfTrips] = useState([]);
-  const [unscheduledTrips, setUnscheduledTrips] = useState([]);
+export default function SchedulingApp({ selectedDate, editable }) {
+  const [listOfTripsByDriver, setListOfTripsByDriver] = useState({});
+  const gridRefs = useRef({});
 
-  const fetchDrivers = async () => {
-    const querySnapshot = await getDocs(collection(db, "Bus Drivers"));
-    const drivers = querySnapshot.docs.map((doc) => doc.id);
-    setListOfDrivers(drivers);
-  };
-
-  const fetchTrips = async (driverId = null) => {
-    const tripsCollectionRef = collection(db, "Dates", selectedDate, "trips");
-    let querySnapshot;
-
-    if (driverId) {
-      const driverTrips = query(
-        tripsCollectionRef,
-        where("bus", "==", driverId)
-      );
-      querySnapshot = await getDocs(driverTrips);
-    } else {
-      querySnapshot = await getDocs(tripsCollectionRef);
-    }
-
-    const trips = querySnapshot.docs.map((doc) => ({
+  const populateListOfTripsByDriver = async () => {
+    const driverQuery = await getDocs(collection(db, "Bus Drivers"));
+    const drivers = driverQuery.docs.map((doc) => doc.id);
+    const tripsQuery = await getDocs(
+      collection(db, "Dates", selectedDate, "trips")
+    );
+    const trips = tripsQuery.docs.map((doc) => ({
       id: doc.id,
       ...doc.data(),
     }));
+    const res = {};
+    res["Unscheduled Trips"] = trips.filter(
+      (trip) => trip.bus === "" || trip.bus === null
+    );
+    drivers.forEach((driverId) => {
+      res[driverId] = trips.filter((trip) => trip.bus === driverId);
+    });
+    setListOfTripsByDriver(res);
+  };
 
-    if (driverId) {
-      setListOfTrips((prevTrips) => [
-        ...prevTrips.filter((trip) => trip.driverId !== driverId),
-        { driverId, trips },
-      ]);
-    } else {
-      setUnscheduledTrips(trips.filter((trip) => trip.bus === ""));
-      setListOfTrips([]);
+  const getRowId = (params) => params.data.id;
+
+  const onGridReady = (params, gridKey) => {
+    gridRefs.current[gridKey] = params.api;
+  };
+
+  const gridDragOver = (event) => {
+    const dragSupported = event.dataTransfer.types.length;
+    if (dragSupported) {
+      event.dataTransfer.dropEffect = "move";
+      event.preventDefault();
     }
   };
 
-  const fetchDriverTrips = async (driverId) => {
-    await fetchTrips(driverId);
+  const gridDrop = async (grid, event) => {
+    event.preventDefault();
+
+    const jsonData = event.dataTransfer.getData("application/json");
+    const data = JSON.parse(jsonData);
+
+    // if data missing or data has no it, do nothing
+    if (!data || data.id == null) {
+      return;
+    }
+    const gridApi = gridRefs.current[grid];
+
+    // do nothing if row is already in the grid, otherwise we would have duplicates
+    const rowAlreadyInGrid = !!gridApi.getRowNode(data.id);
+    if (rowAlreadyInGrid) {
+      return;
+    }
+
+    if (grid === "Unscheduled Trips") {
+      await updateDoc(doc(db, "Dates", selectedDate, "trips", data.id), {
+        bus: "",
+      });
+    } else {
+      await updateDoc(doc(db, "Dates", selectedDate, "trips", data.id), {
+        bus: grid,
+      });
+    }
+    populateListOfTripsByDriver();
   };
 
   useEffect(() => {
-    fetchDrivers();
+    populateListOfTripsByDriver();
   }, []);
 
   useEffect(() => {
-    fetchTrips(); // Fetch unscheduled trips
-    listOfDrivers.forEach((driverId) => {
-      fetchDriverTrips(driverId);
-    });
-  }, [selectedDate, listOfDrivers]);
+    populateListOfTripsByDriver();
+  }, [selectedDate]);
 
   return (
     <div
       className="driver-tables-container"
-      style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "center",
+      }}
     >
-      <div
-        className="driver-table"
-        style={{ width: "25%", marginBottom: "20px", marginRight: "15px" }}
-      >
-        <h3>Unscheduled Trips</h3>
-        <div
-          className="ag-theme-alpine"
-          style={{ height: "400px", width: "100%" }}
-        >
-          <AgGridReact
-            columnDefs={[
-              { headerName: "Time", field: "time" },
-              { headerName: "Description", field: "description" },
-            ]}
-            rowData={unscheduledTrips}
-            suppressHorizontalScroll={true}
-          />
-        </div>
-      </div>
-
-      {listOfDrivers.map((driverId) => {
-        const driverTripData = listOfTrips.find(
-          (tripData) => tripData.driverId === driverId
-        );
-        if (!driverTripData) {
-          return null;
-        }
-
-        const driverTrips = driverTripData.trips;
-        const scheduledDriverTrips = driverTrips.filter(
-          (trip) => trip.bus === driverId
-        );
-
+      {Object.keys(listOfTripsByDriver).map((driverId) => {
+        const driverTripData = listOfTripsByDriver[driverId] || [];
         return (
           <div
             className="driver-table"
             key={driverId}
-            style={{ width: "25%", marginBottom: "20px", marginRight: "15px" }}
+            style={{
+              width: "25%",
+              marginBottom: "20px",
+              marginRight: "15px",
+            }}
           >
-            <h3>{driverId}</h3>
+            <h3> {driverId} </h3>{" "}
             <div
-              className="ag-theme-alpine"
+              className={
+                localStorage.getItem("darkMode") === "true"
+                  ? "ag-theme-alpine-dark"
+                  : "ag-theme-alpine"
+              }
               style={{ height: "400px", width: "100%" }}
+              onDragOver={gridDragOver}
+              onDrop={(e) => gridDrop(driverId, e)}
             >
               <AgGridReact
                 columnDefs={[
-                  { headerName: "Time", field: "time" },
-                  { headerName: "Description", field: "description" },
+                  {
+                    headerName: "Time",
+                    field: "time",
+                    dndSource: editable,
+                  },
+                  {
+                    headerName: "Description",
+                    field: "description",
+                  },
+                  {
+                    headerName: "id",
+                    field: "id",
+                  },
                 ]}
-                rowData={scheduledDriverTrips}
+                rowData={driverTripData}
                 suppressHorizontalScroll={true}
+                onGridReady={(params) => onGridReady(params, driverId)}
+                rowDragManaged={true}
+                animateRows={true}
+                getRowId={getRowId}
               />
             </div>
           </div>
