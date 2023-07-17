fa6f9332ff353fa74f4bb2d51de485dbc40dd07d
DS <yeodisheng@gmail.com>
2023-07-17 04:13:37 +0000

    Fix date validation in drag and drop

--- src/pages/Scheduling/SchedulingApp.js
+++ src/pages/Scheduling/SchedulingApp.js
@@ -1,36 +1,34 @@
-import { AgGridReact } from "ag-grid-react";
-import "ag-grid-community/styles/ag-grid.css";
-import "ag-grid-community/styles/ag-theme-alpine.css";
-import { useEffect, useState, useRef } from "react";
-import { db } from "../../firebase";
-import { collection, getDocs, doc, updateDoc } from "firebase/firestore";
-import { Title } from "../../components/Typography/Title";
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+import { useEffect, useState, useRef } from 'react';
+import { db } from '../../firebase';
+import { doc, updateDoc } from 'firebase/firestore';
+import { Title } from '../../components/Typography/Title';
 import {
     ParseTimeFromFirestoreToString,
     parseDateTimeFromStringToFireStore,
     ParseTimeFromFirestore,
-} from "../../utils/ParseTime";
-import { message } from "antd";
+} from '../../utils/ParseTime';
+import { message } from 'antd';
 
 export default function SchedulingApp({
     selectedDate,
     editable,
     listOfTripsByDriver,
+    drivers,
     updateListOfTripsByDriver,
 }) {
-    const unscheduledTrips = "Unscheduled Trips";
+    const unscheduledTrips = 'Unscheduled Trips';
 
     const [driverDetails, setDriverDetails] = useState({});
     const gridRefs = useRef({});
 
     const populateDriverDetails = async () => {
         const res = {};
-        const driverQuery = await getDocs(collection(db, "Bus Drivers"));
-        driverQuery.docs
-            .map((doc) => doc.data())
-            .forEach((row) => {
-                res[row.busNumber] = { ...row };
-            });
+        drivers.forEach((row) => {
+            res[row.busNumber] = { ...row };
+        });
         setDriverDetails(res);
     };
 
@@ -43,7 +41,7 @@ export default function SchedulingApp({
     const gridDragOver = (event) => {
         const dragSupported = event.dataTransfer.types.length;
         if (dragSupported) {
-            event.dataTransfer.dropEffect = "move";
+            event.dataTransfer.dropEffect = 'move';
             event.preventDefault();
         }
     };
@@ -51,7 +49,7 @@ export default function SchedulingApp({
     const gridDrop = async (driverId, event) => {
         event.preventDefault();
 
-        const jsonData = event.dataTransfer.getData("application/json");
+        const jsonData = event.dataTransfer.getData('application/json');
         const data = JSON.parse(jsonData);
 
         // if data missing or data has no it, do nothing
@@ -68,15 +66,30 @@ export default function SchedulingApp({
 
         // do nothing if it clashes
         if (driverId !== unscheduledTrips) {
-            const newTripDT = parseDateTimeFromStringToFireStore(
+            const newTripStartTime = parseDateTimeFromStringToFireStore(
                 data.startTime,
                 selectedDate
             );
+            const newTripEndTime = parseDateTimeFromStringToFireStore(
+                data.endTime,
+                selectedDate
+            );
+
             for (const trip of listOfTripsByDriver[driverId]) {
-                const tripDT = ParseTimeFromFirestore(trip.startTime);
-                const diffInMinutes = tripDT.diff(newTripDT, "minute");
-                console.log(diffInMinutes);
-                if (diffInMinutes >= -15 && diffInMinutes <= 15) {
+                const tripStartTime = ParseTimeFromFirestore(trip.startTime);
+                const tripEndTime = ParseTimeFromFirestore(trip.endTime);
+                const diffNewStartOldEnd = newTripStartTime.diff(
+                    tripEndTime,
+                    'minute'
+                );
+                const diffNewEndOldStart = newTripEndTime.diff(
+                    tripStartTime,
+                    'minute'
+                );
+                if (
+                    (diffNewStartOldEnd >= -15 && diffNewStartOldEnd <= 15) ||
+                    (diffNewEndOldStart >= -15 && diffNewEndOldStart <= 15)
+                ) {
                     message.error(
                         `${driverId} cannot be scheduled this trip as it is within 15 minutes of another trip.`
                     );
@@ -86,11 +99,11 @@ export default function SchedulingApp({
         }
 
         if (driverId === unscheduledTrips) {
-            await updateDoc(doc(db, "Dates", selectedDate, "trips", data.id), {
-                bus: "",
+            await updateDoc(doc(db, 'Dates', selectedDate, 'trips', data.id), {
+                bus: '',
             });
         } else {
-            await updateDoc(doc(db, "Dates", selectedDate, "trips", data.id), {
+            await updateDoc(doc(db, 'Dates', selectedDate, 'trips', data.id), {
                 bus: driverId,
             });
         }
@@ -98,30 +111,35 @@ export default function SchedulingApp({
     };
 
     useEffect(() => {
-        populateDriverDetails();
-    }, []);
+        if (drivers.length > 0) {
+            populateDriverDetails();
+        }
+    }, [drivers]);
 
     const generateGrid = (driverId) => {
         const driverTrips = listOfTripsByDriver[driverId] || [];
-        const driverTripData = JSON.parse(JSON.stringify(driverTrips)); // Deep copy as reacts somehow calls generateGrid twice
+        const driverTripData = JSON.parse(JSON.stringify(driverTrips)); // Deep copy to not mutate values
 
         driverTripData.map((row) => {
             row.startTime = ParseTimeFromFirestoreToString(row.startTime);
             if (row.startTime2) {
                 row.startTime2 = ParseTimeFromFirestoreToString(row.startTime2);
             }
+            if (row.endTime) {
+                row.endTime = ParseTimeFromFirestoreToString(row.endTime);
+            }
             return row;
         });
 
-        var busSize = "";
-        var contactNumber = "";
-        var remarks = "";
+        var busSize = '';
+        var contactNumber = '';
+        var remarks = '';
         const driverObj = driverDetails[driverId];
         const driverData = { ...driverObj }; // it doesnt work without this i dont know why
         if (driverId !== unscheduledTrips) {
-            busSize = driverData["busSize"];
-            contactNumber = driverData["contactNumber"];
-            remarks = driverData["remarks"];
+            busSize = driverData['busSize'];
+            contactNumber = driverData['contactNumber'];
+            remarks = driverData['remarks'];
         }
 
         return (
@@ -130,31 +148,31 @@ export default function SchedulingApp({
                     {driverId === unscheduledTrips
                         ? `${driverId}`
                         : `${driverId} (${busSize}) HP: ${contactNumber} ${
-                              remarks || ""
+                              remarks || ''
                           }`}
                 </Title>
                 <div
                     className={
-                        localStorage.getItem("darkMode") === "true"
-                            ? "ag-theme-alpine-dark"
-                            : "ag-theme-alpine"
+                        localStorage.getItem('darkMode') === 'true'
+                            ? 'ag-theme-alpine-dark'
+                            : 'ag-theme-alpine'
                     }
-                    style={{ height: "400px", width: "100%" }}
+                    style={{ height: '400px', width: '100%' }}
                     onDragOver={gridDragOver}
                     onDrop={(e) => gridDrop(driverId, e)}
                 >
                     <AgGridReact
                         columnDefs={[
                             {
-                                headerName: "Time",
-                                field: "startTime",
+                                headerName: 'Time',
+                                field: 'startTime',
                                 maxWidth: 100,
                                 dndSource: editable,
                             },
                             {
-                                headerName: "Trip Description",
+                                headerName: 'Trip Description',
                                 flex: 2,
-                                field: "tripDescription",
+                                field: 'tripDescription',
                             },
                         ]}
                         rowData={driverTripData}
@@ -172,15 +190,15 @@ export default function SchedulingApp({
     return (
         <div
             style={{
-                padding: "10px 5px 40px 5px",
+                padding: '10px 5px 40px 5px',
             }}
         >
             <div
                 style={{
-                    position: "fixed",
-                    height: "600px",
-                    width: "400px",
-                    marginLeft: "20px",
+                    position: 'fixed',
+                    height: '600px',
+                    width: '400px',
+                    marginLeft: '20px',
                 }}
             >
                 {generateGrid(unscheduledTrips)}
@@ -188,10 +206,10 @@ export default function SchedulingApp({
             <div
                 className="driver-tables-container"
                 style={{
-                    display: "flex",
-                    flexWrap: "wrap",
-                    justifyContent: "center",
-                    marginLeft: "270px",
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    justifyContent: 'center',
+                    marginLeft: '270px',
                 }}
             >
                 {Object.keys(listOfTripsByDriver).map((driverId, i) => {
@@ -202,10 +220,10 @@ export default function SchedulingApp({
                         <div
                             key={driverId}
                             style={{
-                                height: "400px",
-                                width: "33%",
-                                marginBottom: "40px",
-                                marginRight: "15px",
+                                height: '400px',
+                                width: '33%',
+                                marginBottom: '40px',
+                                marginRight: '15px',
                             }}
                         >
                             {generateGrid(driverId)}
