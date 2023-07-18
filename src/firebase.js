2548cf044d643b6a05ab91f5f4bda1f0f7cf063c
Jun Wei <52122631+jwjwwjwj@users.noreply.github.com>
2023-07-18 05:00:52 +0000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_PROJECTID,
+  apiKey: "AIzaSyDuwfDd3Il7PuRhyJfx-Aowl2swjLmNamE",
+  authDomain: "testing-server-ac4fc.firebaseapp.com",
+  projectId: "testing-server-ac4fc",
   storageBucket: "testing-server-ac4fc.appspot.com",
   messagingSenderId: "517700197493",
   appId: "1:517700197493:web:a93248a3432114bd54ac3c",
-  measurementId: "G-245PGPG8N0",
 };
 
 const app = initializeApp(firebaseConfig);
