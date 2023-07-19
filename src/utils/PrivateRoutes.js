13fb5780749e2ae08d751cfd447cf66b8fb17b51
DS <yeodisheng@gmail.com>
2023-07-19 03:26:42 +0000

    Fix logout on refresh

--- src/utils/PrivateRoutes.js
+++ src/utils/PrivateRoutes.js
@@ -1,9 +1,15 @@
 import { Outlet, Navigate } from "react-router-dom";
-import { auth } from "../firebase";
+import { useAuth } from "./useAuth";
 
 const PrivateRoutes = () => {
-  let user = auth.currentUser;
-  return user ? <Outlet /> : <Navigate to="/login" />;
+  let user = useAuth();
+  return typeof user === "undefined" ? (
+    <></>
+  ) : user ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" />
+  );
 };
 
 export default PrivateRoutes;
