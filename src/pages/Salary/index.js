10ffcba656f33751ea4d6dcf662c75c836d9375a
Jun Wei <52122631+jwjwwjwj@users.noreply.github.com>
2023-07-19 01:26:45 +0000

    Update .gitignore and index.js

--- src/pages/Salary/index.js
+++ src/pages/Salary/index.js
@@ -2,13 +2,18 @@ import React from "react";
 import { Space, DatePicker, Button } from "antd";
 import { SendOutlined } from "@ant-design/icons";
 import { Title } from "../../components/Typography/Title";
+import { db } from "../../firebase.js";
+import { doc, getDoc } from "firebase/firestore";
+const docRef = doc(db, "test_data", "eXzhx4lKoZYBo7C2Wq9g");
+const docSnap = await getDoc(docRef);
 
 const Salary = () => {
   return (
     <>
       <Title>Salary Page</Title>
       <Space>
-        Choose Month: <DatePicker picker="month" />
+        <Title level={5}>Choose Month: </Title>
+        <DatePicker picker="month" />
         <Button type="primary" icon={<SendOutlined />}>
           Generate
         </Button>
