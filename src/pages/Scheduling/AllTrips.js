faa48bc9517977a04178cf9d3072952e0cd550ef
DS <yeodisheng@gmail.com>
2023-07-21 02:21:29 +0000

    Add assign bus to all trips + filter by unassigned

--- src/pages/Scheduling/AllTrips.js
+++ src/pages/Scheduling/AllTrips.js
@@ -1,141 +1,203 @@
-import { useMemo, useRef, useCallback, useState } from 'react';
-import { AgGridReact } from 'ag-grid-react';
-import 'ag-grid-community/styles/ag-grid.css';
-import 'ag-grid-community/styles/ag-theme-alpine.css';
+import { useMemo, useRef, useCallback, useState } from "react";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
 import {
-    ParseDateToFirestore,
-    ParseTimeFromFirestoreToString,
-    ParseTimeToFirestore,
-} from '../../utils/ParseTime';
-import { Title } from '../../components/Typography/Title';
-import { Button, Select, message } from 'antd';
-import { db } from '../../firebase';
-import { collection, updateDoc, doc } from 'firebase/firestore';
-import BusCellRenderer from './BusCellRenderer';
+  ParseDateToFirestore,
+  ParseTimeFromFirestoreToString,
+  ParseTimeToFirestore,
+} from "../../utils/ParseTime";
+import { Title } from "../../components/Typography/Title";
+import { Button, Checkbox, Select, message } from "antd";
+import { db } from "../../firebase";
+import { collection, updateDoc, doc } from "firebase/firestore";
+import BusCellRenderer from "./BusCellRenderer";
+import { CheckOutlined, WarningOutlined } from "@ant-design/icons";
 
 function AllTrips({
-    drivers,
-    subCons,
-    trips,
-    selectedDate,
-    listOfTripsByDriver,
-    dateWithoutDashes,
+  drivers,
+  subCons,
+  trips,
+  selectedDate,
+  listOfTripsByDriver,
+  dateWithoutDashes,
+  updateListOfTripsByDriver,
 }) {
-    const gridRef = useRef();
-    const [rowStatus, setRowStatus] = useState({});
-    const listOfDriverIds = useMemo(() => {
-        return [...drivers, ...subCons].map((driver) => ({
-            label: driver.id,
-            value: driver.id,
-        }));
-    }, [drivers, subCons]);
+  const gridRef = useRef();
+  const [rowStatus, setRowStatus] = useState({});
+  const listOfDriverIds = useMemo(() => {
+    return [...drivers, ...subCons].map((driver) => ({
+      label: driver.id,
+      value: driver.id,
+    }));
+  }, [drivers, subCons]);
 
-    const defaultColDef = useMemo(() => {
-        return {
-            editable: false,
-            sortable: true,
-            suppressMovable: true,
-            flex: 3,
-            minWidth: 100,
-        };
-    }, []);
-
-    const formatTime = (params) => {
-        return ParseTimeFromFirestoreToString(params.value);
+  const defaultColDef = useMemo(() => {
+    return {
+      editable: false,
+      sortable: true,
+      suppressMovable: true,
+      flex: 3,
+      minWidth: 100,
     };
+  }, []);
 
-    const busCellRenderer = (params) => {
-        return (
-            <BusCellRenderer
-                params={params}
-                drivers={drivers}
-                subCons={subCons}
-                listOfDriverIds={listOfDriverIds}
-                listOfTripsByDriver={listOfTripsByDriver}
-                dateWithoutDashes={dateWithoutDashes}
-            />
-        );
-    };
+  const formatTime = (params) => {
+    return ParseTimeFromFirestoreToString(params.value);
+  };
 
-    const columnDefs = [
-        {
-            headerName: 'Start Time',
-            field: 'startTime',
-            flex: 2,
-            valueFormatter: formatTime,
-            maxWidth: 100,
-        },
-        {
-            headerName: 'Customer Name',
-            field: 'customerName',
-            flex: 2,
-        },
-        {
-            headerName: 'Contact Name',
-            field: 'contactName',
-            flex: 2,
-        },
-        {
-            headerName: 'Contact Number',
-            field: 'contactNumber',
-            flex: 2,
-        },
-        {
-            headerName: 'Pick Up',
-            field: 'pickUpPoint',
-            flex: 4,
-        },
-        {
-            headerName: 'Drop Off',
-            field: 'dropOffPoint',
-            flex: 4,
-        },
-        {
-            headerName: 'No. Pax',
-            field: 'numPax',
-            flex: 1,
-        },
-        {
-            headerName: 'No. Bus',
-            field: 'numBus',
-            flex: 1,
-        },
-        {
-            headerName: 'Bus',
-            field: 'bus',
-            flex: 4,
-            cellRenderer: busCellRenderer,
-            autoHeight: true,
-            wrapText: true,
-        },
-    ];
-
-    const exportToExcel = useCallback(() => {
-        gridRef.current.api.exportDataAsCsv();
-    }, []);
-
+  const busCellRenderer = (params) => {
     return (
-        <>
-            <Title level={2}> {selectedDate}</Title>
-            <Button onClick={exportToExcel}>Download</Button>
-            <div
-                className={
-                    localStorage.getItem('darkMode') === 'true'
-                        ? 'ag-theme-alpine-dark'
-                        : 'ag-theme-alpine'
-                }
-                style={{ height: '400px', width: '100%' }}
-            >
-                <AgGridReact
-                    ref={gridRef}
-                    rowData={trips}
-                    columnDefs={columnDefs}
-                    defaultColDef={defaultColDef}
-                    stopEditingWhenCellsLoseFocus={true}
-                />
-            </div>
-        </>
+      <BusCellRenderer
+        params={params}
+        drivers={drivers}
+        subCons={subCons}
+        listOfDriverIds={listOfDriverIds}
+        listOfTripsByDriver={listOfTripsByDriver}
+        dateWithoutDashes={dateWithoutDashes}
+        updateListOfTripsByDriver={updateListOfTripsByDriver}
+      />
     );
+  };
+
+  const numUnassignedRenderer = (params) => {
+    const numUnassigned = params.data.numBus - params.data.numBusAssigned;
+    if (numUnassigned > 0) {
+      return (
+        <span>
+          {`${params.data.numBus - params.data.numBusAssigned}/${
+            params.data.numBus
+          }`}
+        </span>
+      );
+    } else if (numUnassigned < 0) {
+      return <WarningOutlined />;
+    } else {
+      return <CheckOutlined />;
+    }
+  };
+
+  const columnDefs = [
+    {
+      headerName: "Trip Type",
+      field: "type",
+      flex: 3,
+    },
+    {
+      headerName: "Start Time",
+      field: "startTime",
+      flex: 6,
+      valueFormatter: formatTime,
+      maxWidth: 100,
+    },
+    {
+      headerName: "Contact Name",
+      field: "contactName",
+      flex: 6,
+    },
+    {
+      headerName: "Contact Number",
+      field: "contactNumber",
+      flex: 6,
+    },
+    {
+      headerName: "Pick Up",
+      field: "pickUpPoint",
+      flex: 12,
+    },
+    {
+      headerName: "Drop Off",
+      field: "dropOffPoint",
+      flex: 12,
+    },
+    {
+      headerName: "No. Pax",
+      field: "numPax",
+      flex: 3,
+    },
+    {
+      headerName: "No. Bus",
+      field: "numBus",
+      flex: 3,
+    },
+
+    {
+      headerName: "Bus",
+      field: "bus",
+      flex: 16,
+      cellRenderer: busCellRenderer,
+      autoHeight: true,
+      wrapText: true,
+    },
+    {
+      headerName: "",
+      flex: 1,
+      cellRenderer: numUnassignedRenderer,
+      valueGetter: (params) => params.data.numBus - params.data.numBusAssigned,
+      filter: "agNumberColumnFilter",
+      filterParams: {
+        // pass in additional parameters to the Number Filter
+      },
+    },
+  ];
+
+  const exportToExcel = useCallback(() => {
+    gridRef.current.api.exportDataAsCsv();
+  }, []);
+
+  var filtered = false;
+
+  const externalFilterChanged = useCallback((newValue) => {
+    filtered = newValue;
+    gridRef.current.api.onFilterChanged();
+  }, []);
+
+  const isExternalFilterPresent = useCallback(() => {
+    // if ageType is not everyone, then we are filtering
+    return filtered;
+  }, []);
+
+  const doesExternalFilterPass = useCallback(
+    (node) => {
+      if (node.data) {
+        return node.data.numBusAssigned < node.data.numBus;
+      }
+      return true;
+    },
+    [filtered]
+  );
+
+  return (
+    <>
+      <Title level={2}> {selectedDate}</Title>
+      <Button onClick={exportToExcel}>Download</Button>
+      <Checkbox
+        onChange={(e) => {
+          externalFilterChanged(e.target.checked);
+        }}
+      >
+        Show Only Unassigned
+      </Checkbox>
+      <div
+        className={
+          localStorage.getItem("darkMode") === "true"
+            ? "ag-theme-alpine-dark"
+            : "ag-theme-alpine"
+        }
+        style={{ height: "400px", width: "100%" }}
+      >
+        <AgGridReact
+          ref={gridRef}
+          rowData={trips}
+          columnDefs={columnDefs}
+          defaultColDef={defaultColDef}
+          stopEditingWhenCellsLoseFocus={true}
+          isExternalFilterPresent={isExternalFilterPresent}
+          doesExternalFilterPass={doesExternalFilterPass}
+        />
+      </div>
+    </>
+  );
 }
 
 export default AllTrips;
--- src/pages/Scheduling/BusCellRenderer.js
+++ src/pages/Scheduling/BusCellRenderer.js
@@ -1,140 +1,164 @@
-import { Select, message } from 'antd';
-import { useEffect, useState } from 'react';
+import { Select, message } from "antd";
+import { useEffect, useState } from "react";
 import {
-    ParseDateToFirestore,
-    ParseTimeFromFirestoreToString,
-} from '../../utils/ParseTime';
-import { doc, updateDoc } from 'firebase/firestore';
-import { db } from '../../firebase';
+  ParseDateToFirestore,
+  ParseTimeFromFirestoreToString,
+} from "../../utils/ParseTime";
 import {
-    parseDateTimeFromStringToFireStore,
-    ParseTimeFromFirestore,
-} from '../../utils/ParseTime';
+  doc,
+  updateDoc,
+  runTransaction,
+  arrayRemove,
+} from "firebase/firestore";
+import { db } from "../../firebase";
+import {
+  parseDateTimeFromStringToFireStore,
+  ParseTimeFromFirestore,
+} from "../../utils/ParseTime";
 const BusCellRenderer = ({
-    params,
-    listOfDriverIds,
-    drivers,
-    subCons,
-    listOfTripsByDriver,
-    dateWithoutDashes,
+  params,
+  listOfDriverIds,
+  drivers,
+  subCons,
+  listOfTripsByDriver,
+  dateWithoutDashes,
+  updateListOfTripsByDriver,
 }) => {
-    const [driverDetails, setDriverDetails] = useState({});
-    const unscheduledTrips = 'Unscheduled Trips';
-    const [status, setStatus] = useState(
-        params.data.bus.length > params.data.numBus ? 'warning' : ''
+  const [driverDetails, setDriverDetails] = useState({});
+  const unscheduledTrips = "Unscheduled Trips";
+  const [status, setStatus] = useState(
+    params.data.bus.length > params.data.numBus ? "warning" : ""
+  );
+  const [options, setOptions] = useState([]);
+  const [selected, setSelected] = useState([]);
+  const [changed, setChanged] = useState(false);
+
+  useEffect(() => {
+    getOptions(params);
+  }, [listOfTripsByDriver, driverDetails]);
+
+  useEffect(() => {
+    populateDriverDetails();
+  }, [drivers, subCons]);
+
+  const populateDriverDetails = () => {
+    const res = {};
+    drivers.forEach((row) => {
+      res[row.busNumber] = { ...row };
+    });
+    subCons.forEach((row) => {
+      res[row.busNumber] = { ...row };
+    });
+    setDriverDetails(res);
+  };
+
+  const checkTimeClash = (data, driverId) => {
+    if (driverId === unscheduledTrips) return false;
+    // Check for timing clashes with existing trips
+    const newTripStartTime = parseDateTimeFromStringToFireStore(
+      ParseTimeFromFirestoreToString(data.startTime),
+      dateWithoutDashes
     );
-    const [options, setOptions] = useState([]);
-
-    useEffect(() => {
-        setOptions(getOptions(params));
-    }, [listOfTripsByDriver]);
-
-    useEffect(() => {
-        populateDriverDetails();
-    }, [drivers, subCons]);
-
-    const populateDriverDetails = async () => {
-        const res = {};
-        drivers.forEach((row) => {
-            res[row.busNumber] = { ...row };
-        });
-        subCons.forEach((row) => {
-            res[row.busNumber] = { ...row };
-        });
-        setDriverDetails(res);
-    };
-
-    const checkTimeClash = (data, driverId) => {
-        if (driverId === unscheduledTrips) return false;
-        // Check for timing clashes with existing trips
-        const newTripStartTime = parseDateTimeFromStringToFireStore(
-            ParseTimeFromFirestoreToString(data.startTime),
-            dateWithoutDashes
-        );
-        const newTripEndTime = parseDateTimeFromStringToFireStore(
-            ParseTimeFromFirestoreToString(data.endTime),
-            dateWithoutDashes
-        );
-        console.log(listOfTripsByDriver[driverId]);
-        for (const trip of listOfTripsByDriver[driverId] || []) {
-            const tripStartTime = ParseTimeFromFirestore(trip.startTime);
-            const tripEndTime = ParseTimeFromFirestore(trip.endTime);
-            const diffNewStartOldEnd = newTripStartTime.diff(
-                tripEndTime,
-                'minute'
-            );
-            const diffNewEndOldStart = newTripEndTime.diff(
-                tripStartTime,
-                'minute'
-            );
-            if (
-                (diffNewStartOldEnd >= -15 && diffNewStartOldEnd <= 15) ||
-                (diffNewEndOldStart >= -15 && diffNewEndOldStart <= 15)
-            ) {
-                //console.log('CLASH', newTripStartTime, driverId);
-                return true; // Hide grid if there is a timing clash
-            }
-            return false;
-        }
-    };
-
-    const getOptions = (params) => {
-        const options = Object.keys(listOfTripsByDriver).filter((driverId) => {
-            if (driverId === unscheduledTrips) {
-                return false;
-            }
-
-            const driverObj = driverDetails[driverId];
-            const driverData = { ...driverObj };
-            const busSize = driverData.busSize;
-            const rowData = params.data;
-            const paxSize = rowData.numPax;
-
-            if (busSize < paxSize) {
-                return false; // Hide grid if bus size is smaller than required
-            }
-            return !checkTimeClash(rowData, driverId); // Display grid if there are no clashes or bus size issues
-        });
-        console.log(options);
-        return options.map((driverId) => ({
-            label: driverId,
-            value: driverId,
-        }));
-    };
-
-    const handleChange = async (params, value, option, setStatus) => {
-        console.log(value, option);
-        try {
-            const id = params.data.id;
-            const date = ParseDateToFirestore(params.data.date);
-            const tripRef = doc(db, 'Dates', date, 'trips', id);
-            await updateDoc(tripRef, { bus: value });
-            message.success('Successfully Updated Buses');
-        } catch (error) {
-            message.error(error.toString());
-        } finally {
-            getOptions(params);
-            if (value.length > params.data.numBus) {
-                setStatus('warning');
-            } else {
-                setStatus('');
-            }
-        }
-    };
-    return (
-        <Select
-            mode="multiple"
-            style={{ width: '100%', height: '100%' }}
-            placeholder="No Bus Assigned"
-            defaultValue={params.data.bus}
-            onChange={(value, option) =>
-                handleChange(params, value, option, setStatus)
-            }
-            options={options}
-            status={status}
-            // dropdownStyle={} fix coloring?
-        ></Select>
+    const newTripEndTime = parseDateTimeFromStringToFireStore(
+      ParseTimeFromFirestoreToString(data.endTime),
+      dateWithoutDashes
     );
+    for (const trip of listOfTripsByDriver[driverId] || []) {
+      const tripStartTime = ParseTimeFromFirestore(trip.startTime);
+      const tripEndTime = ParseTimeFromFirestore(trip.endTime);
+      const diffNewStartOldEnd = newTripStartTime.diff(tripEndTime, "minute");
+      const diffNewEndOldStart = newTripEndTime.diff(tripStartTime, "minute");
+      if (
+        (diffNewStartOldEnd >= -15 && diffNewStartOldEnd <= 15) ||
+        (diffNewEndOldStart >= -15 && diffNewEndOldStart <= 15)
+      ) {
+        return true; // Hide grid if there is a timing clash
+      }
+    }
+    return false;
+  };
+
+  const getOptions = (params) => {
+    const options = Object.keys(listOfTripsByDriver).filter((driverId) => {
+      if (driverId === unscheduledTrips) {
+        return false;
+      }
+
+      const driverObj = driverDetails[driverId];
+      const driverData = { ...driverObj };
+      const busSize = driverData.busSize;
+      const rowData = params.node.data;
+      const paxSize = rowData.numPax;
+      if (busSize < paxSize) {
+        return false; // Hide grid if bus size is smaller than required
+      }
+      return !checkTimeClash(rowData, driverId); // Display grid if there are no clashes or bus size issues
+    });
+    setOptions(
+      options.map((driverId) => ({
+        label: driverId,
+        value: driverId,
+      }))
+    );
+  };
+
+  const handleChange = async (value) => {
+    setChanged(true);
+    setSelected(value);
+  };
+
+  const onBlur = async (params, e) => {
+    if (!changed) return;
+    const data = params.node.data;
+    try {
+      const id = data.id;
+      const tripRef = doc(db, "Dates", dateWithoutDashes, "trips", id);
+      await updateDoc(tripRef, {
+        bus: selected,
+        numBusAssigned: selected.length,
+      });
+      message.success("Successfully Updated Buses");
+    } catch (error) {
+      message.error(error.toString());
+    } finally {
+      setSelected([]);
+      setChanged(false);
+      updateListOfTripsByDriver();
+    }
+  };
+
+  const onDeselect = async (params, value) => {
+    const data = params.node.data;
+    try {
+      const id = data.id;
+      const tripRef = doc(db, "Dates", dateWithoutDashes, "trips", id);
+      await runTransaction(db, async (transaction) => {
+        const docSnapshot = await transaction.get(tripRef);
+        transaction.update(docSnapshot.ref, {
+          bus: arrayRemove(value),
+          numBusAssigned: docSnapshot.data().numBusAssigned - 1,
+        });
+      });
+      message.success("Successfully Updated Buses");
+    } catch (error) {
+      message.error(error.toString());
+    } finally {
+      updateListOfTripsByDriver();
+    }
+  };
+  return (
+    <Select
+      mode="multiple"
+      style={{ width: "100%", height: "100%" }}
+      placeholder="No Bus Assigned"
+      defaultValue={params.data.bus}
+      onChange={(value) => handleChange(value)}
+      onBlur={(e) => onBlur(params, e)}
+      onDeselect={(value) => onDeselect(params, value)}
+      options={options}
+      status={status}
+    ></Select>
+  );
 };
 
 export default BusCellRenderer;
